453a43e5e7700c40a2b1e27a1c1f1ae417e1170d
Minel <osmanmin@students.zhaw.ch>
2024-03-17 11:21:47 +0100

    closes #1

--- src/main/java/ch/zhaw/youvsyou/controller/FitnessuserController.java
+++ src/main/java/ch/zhaw/youvsyou/controller/FitnessuserController.java
@@ -1,8 +1,13 @@
 package ch.zhaw.youvsyou.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import ch.zhaw.youvsyou.model.Fitnessuser;
 import ch.zhaw.youvsyou.model.FitnessuserCreateDTO;
 import ch.zhaw.youvsyou.repository.FitnessuserRepository;
+
 
 @RestController
 @RequestMapping("/api")
@@ -24,5 +30,21 @@ public class FitnessuserController {
         Fitnessuser fDAO = new Fitnessuser(fDTO.getEmail(), fDTO.getName(), fDTO.getBirthDate(), fDTO.getHeight(), fDTO.getWeight());
         Fitnessuser f = fitnessuserRepository.save(fDAO);
         return new ResponseEntity<>(f, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/fitnessuser")
+    public ResponseEntity<List<Fitnessuser>> getAllFitnessuser() {
+        List<Fitnessuser> allFitnessuser = fitnessuserRepository.findAll();
+        return new ResponseEntity<>(allFitnessuser, HttpStatus.OK);
+    }
+
+    @GetMapping("/fitnessuser/{id}")
+    public ResponseEntity<Fitnessuser> getFitnessuserById(@PathVariable String id) {
+        Optional<Fitnessuser> optFitnessuser = fitnessuserRepository.findById(id);
+        if (optFitnessuser.isPresent()) {
+            return new ResponseEntity<>(optFitnessuser.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
